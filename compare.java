0ed2b20e3bd8e12fa9a14a6da6b640b967d09d3e
alshifa0301 <38190841+alshifa0301@users.noreply.github.com>
2022-03-17 16:26:01 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,18 @@
+package pack1;
+
+public class compare {
+
+	public static void main(String[] args) {
+//Integer Compare.
+		int a=10;
+		int b=50;
+		
+		int c=a>b?a:b;
+		
+		System.out.print("Interger comparision" + c);
+
+//Post Pre Fix
+	
+	}
+
+}
